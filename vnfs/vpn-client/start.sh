e286152adf2ffc667d72793b823bbe145df70545
nodir.qodirov@gmail.com <nodir.qodirov@gmail.com>
2017-06-16 04:13:01 +0000

    updated vpn-client docker image start.sh

--- vnfs/vpn-client/start.sh
+++ vnfs/vpn-client/start.sh
@@ -1,12 +1,31 @@
-#!/bin/bash
+#! /bin/bash
 
-# remove IPs from input/output interface to prepare them for bridging
-ip addr flush dev $IFIN
-ip addr flush dev $IFOUT
+echo "Setting up VPN client VNF"
 
-# bridge interfaces (layer 2) and let snort listen to bridge (IDS mode)
-brctl addbr br0
-brctl addif br0 $IFIN $IFOUT
-ifconfig br0 up
+echo "configuring IPv4 and ARP forwarding ..."
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp
+echo "forwarding configuration complete"
 
-echo 'bridged input and output interfaces. VNF is ready.'
+echo "configuring iptables ..."
+iptables -t nat -A POSTROUTING -o output -j MASQUERADE
+iptables -A FORWARD -i output -o input -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i input -o output -j ACCEPT
+echo "iptables configuration complete"
+
+sleep 2
+
+echo "starting openvpn to the server..."
+sudo openvpn client.ovpn &
+echo "sleep 30s to let openvpn finish config"
+sleep 30
+echo "execute ifconfig to see interface"
+ifconfig
+echo "execute route -n to see routing table"
+route -n
+echo "delete routing entry for server main IP address"
+ip route del 10.0.10.10/32
+echo "execute route -n to see routing table"
+route -n
+
+echo "VPN client VNF ready."
