79368c8673e464527f4be5bdf2e101add00175eb
nodir.qodirov@gmail.com <nodir.qodirov@gmail.com>
2017-06-16 00:57:59 +0000

    fixed nat docker container image

--- vnfs/nat/start.sh
+++ vnfs/nat/start.sh
@@ -1,51 +1,16 @@
 #! /bin/bash
 
-echo "FW container started"
+echo "Setting up NAT VNF"
 
-#echo "start ryu learning switch"
-#ryu-manager --verbose ryu.app.simple_switch_13 ryu.app.ofctl_rest 2>&1 | tee ryu.log &
+echo "configuring IPv4 and ARP forwarding ..."
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/conf/all/proxy_arp
+echo "forwarding configuration complete"
 
+echo "configuring iptables ..."
+iptables -t nat -A POSTROUTING -o output -j MASQUERADE
+iptables -A FORWARD -i output -o input -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i input -o output -j ACCEPT
+echo "iptables configuration complete"
 
-echo "Start ovs"
-service openvswitch-switch start
-
-# Configuration parameters of the switch can for example be configured by a docker environment variable
-# but configuration is not yet implemented in the (dummy) gatekeeper, so we hard code it for now
-# also controller ip address should be configured by a service controller after vnfs are deployed and assigned an ip address
-NAME="ovs1"
-OVS_DPID="0000000000000001"
-
-# declare an array variable holding the ovs port names
-# the interfaces are expected to be configured from the vnfd or nsd
-declare -a PORTS=("input" "output")
-
-echo "setup ovs bridge"
-ovs-vsctl add-br $NAME
-ovs-vsctl set bridge $NAME datapath_type=netdev
-ovs-vsctl set bridge $NAME protocols=OpenFlow10,OpenFlow12,OpenFlow13
-ovs-vsctl set-fail-mode $NAME secure
-ovs-vsctl set bridge $NAME other_config:disable-in-band=true
-ovs-vsctl set bridge $NAME other-config:datapath-id=$OVS_DPID
-
-## now loop through the PORTS array
-for i in "${PORTS[@]}"
-do
-   echo "added port $i to switch $NAME"
-   ovs-vsctl add-port $NAME $i
-   # or do whatever with individual element of the array
-done
-
-
-# configuration after startup (needs CONTROLLER_IP):
-# use localhost as interface for ryu <-> ovs 
-# since both are running in the same VNF
-#CONTROLLER_IP="127.0.0.1"
-#CONTROLLER="tcp:$CONTROLLER_IP:6633"
-#ovs-vsctl set-controller $NAME $CONTROLLER
-
-sleep 2
-echo "setup generic forwarding for PCAP traffic"
-ovs-ofctl add-flow ovs1 'priority=2,in_port=1,action=output:2'
-sleep 1
-ovs-ofctl add-flow ovs1 'priority=2,in_port=2,action=output:1'
-
+echo "NAT VNF ready"
