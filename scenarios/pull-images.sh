644954e687347e6654af65ee4754f5f0da33a4f1
nodir.qodirov@gmail.com <nodir.qodirov@gmail.com>
2017-07-04 01:05:33 +0000

    minor: fixed typo on fw docker image name

--- scenarios/pull-images.sh
+++ scenarios/pull-images.sh
@@ -4,7 +4,7 @@ echo "start pulling latest docker images"
 
 sudo docker pull knodir/client
 sudo docker pull knodir/nat
-sudo docker pull knodir/sonate-fw-vnf
+sudo docker pull knodir/sonata-fw-vnf
 sudo docker pull knodir/snort-trusty
 sudo docker pull knodir/snort-xenial
 sudo docker pull knodir/vpn-client
